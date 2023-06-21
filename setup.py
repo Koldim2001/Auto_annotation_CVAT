23d73e4c73e44c85a1bd047ffac032d367710759
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2023-06-21 23:16:50 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name="auto_annotate",
+    version="1.0",
+    packages=find_packages(),
+    long_description="Производит создание файла аннотации YOLO 1.1 для CVAT",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["auto_annotate=main:main"],
+    },
+    install_requires=[
+        "torch",
+        "click",
+        "ultralytics",
+        "opencv-python",
+        'gdown'
+    ],
+)
