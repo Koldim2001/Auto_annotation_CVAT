ec03546968dd97ab6bf88046b2285f5803a77c2b
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2023-06-21 22:48:49 +0300

    main file was created

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# annotation_making_cvat
-Создание автоматической аннотации с использованием предобученной модели YOLOv5
+# Создание автоматической аннотации в CVAT с использованием предобученной модели YOLOv5
+
+### Пример скрипта для запуска кода:
+python main.py --img_folder=image_folder
+
+#### Опции для парсинга аргументоов в cli:
+  --img_folder (TEXT)    &emsp;      Папка с изображениями (task из CVAT)</br>
+  --weights (TEXT)     &emsp;         Путь к натренированным весам yolov5</br>
+  --annotation_zip_file (TEXT) &emsp;  Имя zip архива аннотаций для CVAT формата YOLO</br>
+  --conf (FLOAT)     &emsp;         Порог уверенности классификатора</br>
+  --iou (FLOAT)        &emsp;         Порог iou на non max suppression</br>
+  --help          &emsp;            Покажет существующие варианты парсинга аргументов</br>
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import shutil
 import zipfile
@@ -8,59 +7,166 @@ import click
 import torch
 import cv2
 
+def download_models(folder_name='models'):
+    '''
+    Функция для загрузки моделей обученных нейронных сетей в папку folder_name 
+    из моего гугл диска
+    '''
+    output = folder_name + "/bag_belt_yolo.pt"
+
+    if not os.path.exists(output):
+        # Создание пустой папки 
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+
+        print('Загружаю модель нейронной сети:')
+        url = "https://drive.google.com/uc?id=1p2ErNFGtcN9Gly3ewy1UAz68wAKxSFW3"
+
+        gdown.download(url, output, quiet=False)
+
+
+def save_annotations_to_yolo_format(output_file, results, height_max, width_max):
+    with open(output_file, 'w') as f:
+        for result in results.xyxy[0]:
+            label = result[-1]
+            xmin, ymin, xmax, ymax = result[:4]
+
+            # Получение координат центра объекта и его ширины и высоты
+            width = xmax - xmin
+            height = ymax - ymin
+            x_center = xmin + width / 2
+            y_center = ymin + height / 2
+
+            # Запись аннотации в формате YOLO 1.1
+            label = int(label)
+            x_center_formatted = '{:.6f}'.format(x_center.item() / width_max)
+            y_center_formatted = '{:.6f}'.format(y_center.item() / height_max)
+            width_formatted = '{:.6f}'.format(width.item() / width_max)
+            height_formatted = '{:.6f}'.format(height.item() / height_max)
+
+            line = f"{label} {x_center_formatted} {y_center_formatted} {width_formatted} {height_formatted}\n"
+            f.write(line)
+
+
+def copy_files_to_folder(source_folder, destination_folder):
+    # Создание пустой папки
+    os.makedirs(destination_folder, exist_ok=True)
+
+    # Копирование файлов в папку
+    for file_name in os.listdir(source_folder):
+        source_file = os.path.join(source_folder, file_name)
+        destination_file = os.path.join(destination_folder, file_name)
+        shutil.copy2(source_file, destination_file)
+
+
+def delete_folder(folder):
+    # Удаление папки со всем содержимым
+    shutil.rmtree(folder)
+
+
+def create_zip_archive(folder_path, output_path):
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, arcname=os.path.relpath(file_path, folder_path))
+
+
 @click.command()
 @click.option(
-    "--split",
-    help="A percentage of a split, e.g. 0.9 means split 0.9 for train and 0.1 for test",
-    default=0.7,
+    "--img_folder",
+    default="images",
+    help="Папка с изображениями (task из CVAT)",
+    type=str,
+)
+@click.option(
+    "--weights",
+    default="models/bag_belt_yolo.pt",
+    help="Путь к натренированным весам yolov5",
+    type=str,
+)
+@click.option(
+    "--annotation_zip_file",
+    default="annotations.zip",
+    help="Имя zip архива аннотаций для CVAT формата YOLO",
+    type=str,
+)
+@click.option(
+    "--conf",
+    help="Порог уверенности классификатора",
+    default=0.15,
     type=float,
 )
 @click.option(
-    "--label_tfrms",
-    help="Label union with another existed in dataset (example: 'head->hood,helmet->hat')",
-    default=None,
-    type=str,
-)
-@click.option(
-    "--train_folder",
-    default="obj_Train_data",
-    help="Folder with Train subset inside cvat path (default obj_Train_data)",
-    type=str,
-)
-@click.option(
-    "--val_folder",
-    default="obj_Validation_data",
-    help="Folder with Val subset inside cvat path (default obj_Validation_data)",
-    type=str,
-)
-@click.option(
-    "--test_folder",
-    default="obj_Test_data",
-    help="Folder with Test subset inside cvat path (default obj_Test_data)",
-    type=str,
-)
-@click.option("--img_format", default="png", help="Format of images", type=str)
-@click.option(
-    "--percentage_empty",
-    default=10,
-    help="Percentage of images without any labels in relation to full dataset size",
+    "--iou",
+    help="Порог iou на non max suppression",
+    default=0.4,
     type=float,
-)
-@click.option(
-    "--classes",
-    default="keep-all",
-    help="Classes which labels to keep (e.g. 'A|C')",
-    type=str,
 )
 def main(**kwargs):
     # ------------------ ARG parse ------------------
-    CVAT_input_folder = kwargs["cvat"]
-    mode = kwargs["mode"]
-    output_folder = kwargs["output_folder"]
-    split = kwargs["split"]
-    train_folder = kwargs["train_folder"]
-    val_folder = kwargs["val_folder"]
+    conf = kwargs["conf"]
+    iou = kwargs["iou"]
+    zip_path = kwargs["annotation_zip_file"]
+    weights_path = kwargs["weights"]
+    image_folder = kwargs["img_folder"]
+    output_folder = 'results/obj_train_data'
+   
+    # Загрузка модели YOLOv5
+    download_models()
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path)
+    # Установка порога по уверенности и подавления
+    model.conf = conf  # Порог по уверенности
+    model.iou = iou  # Порог подавления
 
+    # Создание папки для сохранения аннотаций, если она не существует
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Обработка изображений и сохранение аннотаций в формате YOLO 1.1
+    for image_file in os.listdir(image_folder):
+        image_path = os.path.join(image_folder, image_file)
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        height_max, width_max = image.shape[0], image.shape[1]
+
+        # Инференс с использованием модели YOLOv5
+        results = model(image)
+
+        # Путь к файлу аннотации
+        annotation_file = os.path.join(output_folder, f"{os.path.splitext(image_file)[0]}.txt")
+
+        # Сохранение аннотаций в формате YOLO 1.1
+        save_annotations_to_yolo_format(annotation_file, results, height_max, width_max)
+
+    print(f"Аннотации сохранены")
+
+    train_file = 'results/train.txt'  # Имя файла train.txt
+    # Создание пустого файла train.txt, если он не существует
+    if not os.path.exists(train_file):
+        open(train_file, 'w').close()
+
+    # Запись имен файлов в train.txt
+    with open(train_file, 'w') as f:
+        for image_file in os.listdir(image_folder):
+            f.write('data/obj_train_data/' + image_file + '\n')
+
+    # Добавим в папку 2 файла с конфигами модели (их стоит править если меняем классы модели)
+    # Путь к папке, из которой нужно скопировать файлы
+    source_folder = 'extra_data'
+    # Путь к папке, в которую нужно скопировать файлы
+    destination_folder = 'results'
+
+    # Копирование файлов в папку
+    copy_files_to_folder(source_folder, destination_folder)
+
+    # Теперь архивируем итоговую аннотационную папку
+    # Указать путь к папке для сохранения zip-архива
+    zip_path = 'annotations.zip'
+
+    create_zip_archive(destination_folder, zip_path)
+
+    # Удаление папки вспомогательной (results) со всем содержимым
+    delete_folder(destination_folder)
 
 
 if __name__ == "__main__":
